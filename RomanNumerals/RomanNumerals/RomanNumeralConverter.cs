ae05e57e1875e0972f70dd12f4483ee61774f25e
Seth McCarthy <seth.mccarthy@olo.com>
2022-07-10 05:39:35 -0700

    Refactor
    Use a DTO to hold definition info for each symbol, iterate over it.

--- RomanNumerals/RomanNumerals/RomanNumeralConverter.cs
+++ RomanNumerals/RomanNumerals/RomanNumeralConverter.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using NUnit.Framework;
 
 namespace RomanNumerals;
 
@@ -8,27 +9,39 @@ public class RomanNumeralConverter
     {
         var amount = 0;
 
-        (amount, roman) = CountTermIfPresent(amount, roman, "III", 3);
-        (amount, roman) = CountTermIfPresent(amount, roman, "II", 2);
-        (amount, roman) = CountTermIfPresent(amount, roman, "I", 1);
-        (amount, roman) = CountTermIfPresent(amount, roman, "IV", 4);
-        (amount, roman) = CountTermIfPresent(amount, roman, "V", 5);
-        (amount, roman) = CountTermIfPresent(amount, roman, "IX", 9);
-        (amount, roman) = CountTermIfPresent(amount, roman, "X", 10);
-        (amount, roman) = CountTermIfPresent(amount, roman, "X", 10);
-        (amount, roman) = CountTermIfPresent(amount, roman, "X", 10);
+        foreach (var definition in new [] {I, IV, V, IX, X})
+        {
+            (amount, roman) = CountTermIfPresent(amount, roman, definition);
+        }
 
         return amount;
     }
 
-    private static (int, string) CountTermIfPresent(int accumulator, string origin, string term, int termAmount)
+    private static (int, string) CountTermIfPresent(int accumulator, string origin, NumeralDefinition definition)
     {
-        if (origin.EndsWith(term))
+        for (int i = 0; i < definition.Repetitions; i++)
         {
-            accumulator += termAmount;
-            origin = origin.Substring(0, origin.Length - term.Length);
+            if (origin.EndsWith(definition.Glyph))
+            {
+                accumulator += definition.Value;
+                origin = origin.Substring(0, origin.Length - definition.Length);
+            }            
         }
-        
+
         return (accumulator, origin);
+    }
+
+    private static readonly NumeralDefinition I = new() { Glyph = "I", Value = 1, Repetitions = 3 };
+    private static readonly NumeralDefinition IV = new() { Glyph = "IV", Value = 4, Repetitions = 1 };
+    private static readonly NumeralDefinition V = new() { Glyph = "V", Value = 5, Repetitions = 1 };
+    private static readonly NumeralDefinition IX = new() { Glyph = "IX", Value = 9, Repetitions = 1 };
+    private static readonly NumeralDefinition X = new() { Glyph = "X", Value = 10, Repetitions = 3 };
+
+    private class NumeralDefinition
+    {
+        public string Glyph;
+        public int Value;
+        public int Repetitions;
+        public int Length => Glyph.Length;
     }
 }
