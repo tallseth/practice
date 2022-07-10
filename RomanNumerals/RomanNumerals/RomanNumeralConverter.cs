8aaab3bca6e4246df011b3b079da981a8a78f6e4
Seth McCarthy <seth.mccarthy@olo.com>
2022-07-09 20:42:03 -0700

    RomanNumerals: scaffolding
    Just initial creation of files, build and test setup.

---
+++ RomanNumerals/RomanNumerals/RomanNumeralConverter.cs
@@ -0,0 +1,6 @@
+﻿namespace RomanNumerals;
+
+public class RomanNumeralConverter
+{
+    
+}
---
+++ RomanNumerals/RomanNumerals/RomanNumeralConverterTests.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace RomanNumerals;
+
+public class RomanNumeralConverterTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
