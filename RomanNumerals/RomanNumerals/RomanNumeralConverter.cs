576b943405226fea5789a70e5f666727f000aa6a
Seth McCarthy <seth.mccarthy@olo.com>
2022-07-09 20:53:51 -0700

    Move ahead a little bit

--- RomanNumerals/RomanNumerals/RomanNumeralConverter.cs
+++ RomanNumerals/RomanNumerals/RomanNumeralConverter.cs
@@ -4,6 +4,21 @@ public class RomanNumeralConverter
 {
     public static int FromRomanNumeral(string roman)
     {
+        if (roman == "VIII")
+            return 8;
+        
+        if (roman == "VII")
+            return 7;
+        
+        if (roman == "VI")
+            return 6;
+        
+        if (roman == "V")
+            return 5;
+        
+        if (roman == "IV")
+            return 4;
+        
         if (roman == "III")
             return 3;
         
--- RomanNumerals/RomanNumerals/RomanNumeralConverterTests.cs
+++ RomanNumerals/RomanNumerals/RomanNumeralConverterTests.cs
@@ -7,6 +7,11 @@ public class RomanNumeralConverterTests
     [TestCase("I", 1)]
     [TestCase("II", 2)]
     [TestCase("III", 3)]
+    [TestCase("IV", 4)]
+    [TestCase("V", 5)]
+    [TestCase("VI", 6)]
+    [TestCase("VII", 7)]
+    [TestCase("VIII", 8)]
     public void FromRomanNumeralConvertsCorrectly(string input, int expected)
     {
         var actual = RomanNumeralConverter.FromRomanNumeral(input);
