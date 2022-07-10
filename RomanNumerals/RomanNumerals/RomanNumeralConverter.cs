12c15293580bcfeb12d414a2637a8d7e433174c5
Seth McCarthy <seth.mccarthy@olo.com>
2022-07-10 05:55:44 -0700

    Refactor: auto calculate repetitions

--- RomanNumerals/RomanNumerals/RomanNumeralConverter.cs
+++ RomanNumerals/RomanNumerals/RomanNumeralConverter.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using NUnit.Framework;
 
 namespace RomanNumerals;
@@ -31,23 +32,28 @@ public class RomanNumeralConverter
         return (accumulator, origin);
     }
 
-    private static readonly NumeralDefinition I = new() { Glyph = "I", Value = 1, Repetitions = 3 };
-    private static readonly NumeralDefinition IV = new() { Glyph = "IV", Value = 4, Repetitions = 1 };
-    private static readonly NumeralDefinition V = new() { Glyph = "V", Value = 5, Repetitions = 1 };
-    private static readonly NumeralDefinition IX = new() { Glyph = "IX", Value = 9, Repetitions = 1 };
-    private static readonly NumeralDefinition X = new() { Glyph = "X", Value = 10, Repetitions = 3 };
-    private static readonly NumeralDefinition XL = new() { Glyph = "XL", Value = 40, Repetitions = 1 };
-    private static readonly NumeralDefinition L = new() { Glyph = "L", Value = 50, Repetitions = 1 };
-    private static readonly NumeralDefinition XC = new() { Glyph = "XC", Value = 90, Repetitions = 1 };
-    private static readonly NumeralDefinition C = new() { Glyph = "C", Value = 100, Repetitions = 3 };
-    private static readonly NumeralDefinition CM = new() { Glyph = "CM", Value = 900, Repetitions = 1 };
-    private static readonly NumeralDefinition M = new() { Glyph = "M", Value = 1000, Repetitions = 3 };
+    private static readonly NumeralDefinition I = new() { Glyph = "I", Value = 1 };
+    private static readonly NumeralDefinition IV = new() { Glyph = "IV", Value = 4 };
+    private static readonly NumeralDefinition V = new() { Glyph = "V", Value = 5 };
+    private static readonly NumeralDefinition IX = new() { Glyph = "IX", Value = 9 };
+    private static readonly NumeralDefinition X = new() { Glyph = "X", Value = 10 };
+    private static readonly NumeralDefinition XL = new() { Glyph = "XL", Value = 40 };
+    private static readonly NumeralDefinition L = new() { Glyph = "L", Value = 50 };
+    private static readonly NumeralDefinition XC = new() { Glyph = "XC", Value = 90 };
+    private static readonly NumeralDefinition C = new() { Glyph = "C", Value = 100 };
+    private static readonly NumeralDefinition CM = new() { Glyph = "CM", Value = 900 };
+    private static readonly NumeralDefinition M = new() { Glyph = "M", Value = 1000 };
 
     private class NumeralDefinition
     {
         public string Glyph;
         public int Value;
-        public int Repetitions;
+        public int Repetitions => ValueIsPowerOfTen() ? 3 : 1;
         public int Length => Glyph.Length;
+        
+        private bool ValueIsPowerOfTen()
+        {
+            return Math.Abs(Math.Log10(Value) - Math.Floor(Math.Log10(Value))) < 0.01;
+        }
     }
 }
