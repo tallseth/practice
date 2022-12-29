ccb2f0aff688d71a7b0d3fdb623d783f8cc86307
Seth McCarthy <seth@letshighlight.com>
2022-12-29 06:51:36 -0800

    SendEmail: initial commit
    Setup for creating the lambda, with boilerplate example code, and a
    simple test to make sure the all the setup was complete.
    
    Created two versions, which are the same for now.  One for implementing
    and testing with mocks, one for using nullables.

---
+++ SendEmail/SendEmail.Mocks/Function.cs
@@ -0,0 +1,25 @@
+﻿using Amazon.Lambda.Core;
+using Amazon.Lambda.SQSEvents;
+
+namespace SendEmail.Mocks;
+
+public class Function
+{
+    public async Task<SQSBatchResponse> FunctionHandler(SQSEvent evnt, ILambdaContext context)
+    {
+        foreach(var message in evnt.Records)
+        {
+            await ProcessMessageAsync(message, context);
+        }
+
+        return new SQSBatchResponse();
+    }
+
+    private async Task ProcessMessageAsync(SQSEvent.SQSMessage message, ILambdaContext context)
+    {
+        context.Logger.LogInformation($"Processed message {message.Body}");
+
+        // TODO: Do interesting work based on the new message
+        await Task.CompletedTask;
+    }
+}
---
+++ SendEmail/SendEmail.Nullables.Tests/FunctionTests.cs
@@ -0,0 +1,20 @@
+﻿using Amazon.Lambda.SQSEvents;
+using Amazon.Lambda.TestUtilities;
+using NUnit.Framework;
+
+namespace SendEmail.Nullables.Tests;
+
+public class FunctionTests
+{
+    [Test]
+    public async Task EmptyBatchDoesNotFail()
+    {
+        var function = new Function();
+        var input = new SQSEvent { Records = new List<SQSEvent.SQSMessage>() };
+        var context = new TestLambdaContext();
+
+        var result = await function.FunctionHandler(input, context);
+        
+        Assert.That(result.BatchItemFailures, Is.Empty);
+    }
+}
