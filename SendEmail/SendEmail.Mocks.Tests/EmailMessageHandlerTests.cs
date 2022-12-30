f97893926cc9316500c88bde5934cbcb0e85bc64
Seth McCarthy <seth@letshighlight.com>
2022-12-30 10:58:47 -0800

    Add isolated tests for extracted classes
    Keep existing tests for a safety net, for now

---
+++ SendEmail/SendEmail.Mocks.Tests/EmailMessageHandlerTests.cs
@@ -0,0 +1,21 @@
+﻿using Amazon.SimpleEmail;
+using Amazon.SimpleEmail.Model;
+using Moq;
+using NUnit.Framework;
+
+namespace SendEmail.Mocks.Tests;
+
+public class EmailMessageHandlerTests
+{
+    [Test]
+    public async Task DelegatesToAws()
+    {
+        var aws = new Mock<IAmazonSimpleEmailService>();
+        var handler = new EmailMessageHandler(aws.Object);
+        var request = new SendTemplatedEmailRequest { Source = Guid.NewGuid().ToString() };
+
+        await handler.Handle(request);
+
+        aws.Verify(s=>s.SendTemplatedEmailAsync(request, It.IsAny<CancellationToken>()), Times.Once());
+    }
+}
