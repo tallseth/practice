1b7ca3b21c5e5ad35c8bf63124bb70225b5535ae
Seth McCarthy <seth@letshighlight.com>
2022-12-30 10:42:24 -0800

    simple extract files automated refactor

--- SendEmail/SendEmail.Mocks/Function.cs
+++ SendEmail/SendEmail.Mocks/Function.cs
@@ -1,5 +1,4 @@
-﻿using System.Text.Json;
-using Amazon.Lambda.Core;
+﻿using Amazon.Lambda.Core;
 using Amazon.Lambda.SQSEvents;
 using Amazon.SimpleEmail;
 using Amazon.SimpleEmail.Model;
@@ -21,59 +20,3 @@ public class Function
         return await _adapter.Adapt(input);
     }
 }
-
-public interface ISqsAdapter
-{
-    Task<SQSBatchResponse> Adapt(SQSEvent sqsEvent);
-}
-
-public class SqsAdapter<T> : ISqsAdapter
-{
-    private readonly IMessageHandler<T> _messageHandler;
-
-    public SqsAdapter(IMessageHandler<T> messageHandler)
-    {
-        _messageHandler = messageHandler;
-    }
-
-    public async Task<SQSBatchResponse> Adapt(SQSEvent sqsEvent)
-    {
-        var response = new SQSBatchResponse();
-        
-        foreach(var message in sqsEvent.Records)
-        {
-            var value = JsonSerializer.Deserialize<T>(message.Body);
-            try
-            {
-                await _messageHandler.Handle(value);
-            }
-            catch
-            {
-                response.BatchItemFailures.Add(new SQSBatchResponse.BatchItemFailure{ ItemIdentifier = message.MessageId });
-            }
-        }
-
-        return response;
-    }
-}
-
-public interface IMessageHandler<T>
-{
-    Task Handle(T message);
-}
-
-public class EmailMessageHandler : IMessageHandler<SendTemplatedEmailRequest>
-{
-    private readonly IAmazonSimpleEmailService _emailService;
-
-    public EmailMessageHandler(IAmazonSimpleEmailService emailService)
-    {
-        _emailService = emailService;
-    }
-
-    public Task Handle(SendTemplatedEmailRequest message)
-    {
-        return _emailService.SendTemplatedEmailAsync(message);
-    }
-}
-
---
+++ SendEmail/SendEmail.Mocks/SqsAdapter.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.Json;
+using Amazon.Lambda.SQSEvents;
+
+namespace SendEmail.Mocks;
+
+public interface ISqsAdapter
+{
+    Task<SQSBatchResponse> Adapt(SQSEvent sqsEvent);
+}
+
+public class SqsAdapter<T> : ISqsAdapter
+{
+    private readonly IMessageHandler<T> _messageHandler;
+
+    public SqsAdapter(IMessageHandler<T> messageHandler)
+    {
+        _messageHandler = messageHandler;
+    }
+
+    public async Task<SQSBatchResponse> Adapt(SQSEvent sqsEvent)
+    {
+        var response = new SQSBatchResponse();
+        
+        foreach(var message in sqsEvent.Records)
+        {
+            var value = JsonSerializer.Deserialize<T>(message.Body);
+            try
+            {
+                await _messageHandler.Handle(value);
+            }
+            catch
+            {
+                response.BatchItemFailures.Add(new SQSBatchResponse.BatchItemFailure{ ItemIdentifier = message.MessageId });
+            }
+        }
+
+        return response;
+    }
+}
