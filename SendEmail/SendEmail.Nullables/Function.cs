75aa41523377458728a33df47ee9051f93fb6e38
Seth McCarthy <seth@letshighlight.com>
2022-12-30 13:09:35 -0800

    split files

--- SendEmail/SendEmail.Nullables/Function.cs
+++ SendEmail/SendEmail.Nullables/Function.cs
@@ -1,5 +1,4 @@
-﻿using System.Text.Json;
-using Amazon.Lambda.Core;
+﻿using Amazon.Lambda.Core;
 using Amazon.Lambda.SQSEvents;
 using Amazon.SimpleEmail.Model;
 
@@ -23,69 +22,4 @@ public class Function
     {
         return await _sqsAdapter.Adapt(input);
     }
-    
-    public interface ISqsAdapter
-    {
-        Task<SQSBatchResponse> Adapt(SQSEvent sqsEvent);
-    }
-
-    public class SqsAdapter<T> : ISqsAdapter
-    {
-        private readonly IMessageHandler<T> _messageHandler;
-
-        public SqsAdapter(IMessageHandler<T> messageHandler)
-        {
-            _messageHandler = messageHandler;
-        }
-
-        public async Task<SQSBatchResponse> Adapt(SQSEvent sqsEvent)
-        {
-            var response = new SQSBatchResponse();
-        
-            foreach(var message in sqsEvent.Records)
-            {
-                var value = JsonSerializer.Deserialize<T>(message.Body);
-                try
-                {
-                    await _messageHandler.Handle(value);
-                }
-                catch
-                {
-                    response.BatchItemFailures.Add(new SQSBatchResponse.BatchItemFailure{ ItemIdentifier = message.MessageId });
-                }
-            }
-
-            return response;
-        }
-    }
-
-    public interface IMessageHandler<T>
-    {
-        Task Handle(T message);
-    }
-
-    public class EmailMessageHandler : IMessageHandler<SendTemplatedEmailRequest>
-    {
-        private readonly EmailWrapper _emailWrapper;
-
-        private EmailMessageHandler(EmailWrapper emailWrapper)
-        {
-            _emailWrapper = emailWrapper;
-        }
-
-        public static EmailMessageHandler Create()
-        {
-            return Create(EmailWrapper.Create());
-        }
-        
-        public static EmailMessageHandler Create(EmailWrapper wrapper)
-        {
-            return new EmailMessageHandler(wrapper);
-        }
-
-        public Task Handle(SendTemplatedEmailRequest message)
-        {
-            return _emailWrapper.SendTemplatedEmail(message);
-        }
-    }    
 }
---
+++ SendEmail/SendEmail.Nullables/SqsAdapter.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json;
+using Amazon.Lambda.SQSEvents;
+
+namespace SendEmail.Nullables;
+
+public class SqsAdapter<T>
+{
+    private readonly IMessageHandler<T> _messageHandler;
+
+    public SqsAdapter(IMessageHandler<T> messageHandler)
+    {
+        _messageHandler = messageHandler;
+    }
+
+    public async Task<SQSBatchResponse> Adapt(SQSEvent sqsEvent)
+    {
+        var response = new SQSBatchResponse();
+    
+        foreach(var message in sqsEvent.Records)
+        {
+            var value = JsonSerializer.Deserialize<T>(message.Body);
+            try
+            {
+                await _messageHandler.Handle(value);
+            }
+            catch
+            {
+                response.BatchItemFailures.Add(new SQSBatchResponse.BatchItemFailure{ ItemIdentifier = message.MessageId });
+            }
+        }
+
+        return response;
+    }
+}
