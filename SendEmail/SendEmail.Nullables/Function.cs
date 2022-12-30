845cf5b356f3e42811a3278edf517357925b9523
Seth McCarthy <seth@letshighlight.com>
2022-12-30 13:05:50 -0800

    SendEmail.Nullable: extract SQSAdapter

--- SendEmail/SendEmail.Nullables/Function.cs
+++ SendEmail/SendEmail.Nullables/Function.cs
@@ -7,7 +7,7 @@ namespace SendEmail.Nullables;
 
 public class Function
 {
-    private readonly EmailWrapper _emailWrapper;
+    private readonly SqsAdapter<SendTemplatedEmailRequest> _sqsAdapter;
 
     public Function() : this(EmailWrapper.Create())
     {
@@ -16,26 +16,76 @@ public class Function
 
     public Function(EmailWrapper emailWrapper)
     {
-        _emailWrapper = emailWrapper;
+        _sqsAdapter = new SqsAdapter<SendTemplatedEmailRequest>(EmailMessageHandler.Create(emailWrapper));
     }
     
     public async Task<SQSBatchResponse> FunctionHandler(SQSEvent input, ILambdaContext _)
     {
-        var batchResponse = new SQSBatchResponse();
+        return await _sqsAdapter.Adapt(input);
+    }
+    
+    public interface ISqsAdapter
+    {
+        Task<SQSBatchResponse> Adapt(SQSEvent sqsEvent);
+    }
 
-        foreach (var record in input.Records)
+    public class SqsAdapter<T> : ISqsAdapter
+    {
+        private readonly IMessageHandler<T> _messageHandler;
+
+        public SqsAdapter(IMessageHandler<T> messageHandler)
         {
-            var request = JsonSerializer.Deserialize<SendTemplatedEmailRequest>(record.Body);
-            try
+            _messageHandler = messageHandler;
+        }
+
+        public async Task<SQSBatchResponse> Adapt(SQSEvent sqsEvent)
+        {
+            var response = new SQSBatchResponse();
+        
+            foreach(var message in sqsEvent.Records)
             {
-                await _emailWrapper.SendTemplatedEmail(request);
+                var value = JsonSerializer.Deserialize<T>(message.Body);
+                try
+                {
+                    await _messageHandler.Handle(value);
+                }
+                catch
+                {
+                    response.BatchItemFailures.Add(new SQSBatchResponse.BatchItemFailure{ ItemIdentifier = message.MessageId });
+                }
             }
-            catch
-            {
-                batchResponse.BatchItemFailures.Add(new SQSBatchResponse.BatchItemFailure { ItemIdentifier = record.MessageId });
-            }
+
+            return response;
+        }
+    }
+
+    public interface IMessageHandler<T>
+    {
+        Task Handle(T message);
+    }
+
+    public class EmailMessageHandler : IMessageHandler<SendTemplatedEmailRequest>
+    {
+        private readonly EmailWrapper _emailWrapper;
+
+        private EmailMessageHandler(EmailWrapper emailWrapper)
+        {
+            _emailWrapper = emailWrapper;
+        }
+
+        public static EmailMessageHandler Create()
+        {
+            return Create(EmailWrapper.Create());
         }
         
-        return batchResponse;
-    }
+        public static EmailMessageHandler Create(EmailWrapper wrapper)
+        {
+            return new EmailMessageHandler(wrapper);
+        }
+
+        public Task Handle(SendTemplatedEmailRequest message)
+        {
+            return _emailWrapper.SendTemplatedEmail(message);
+        }
+    }    
 }
