da89bb1f401b98909741a5ad5dac54a82755d229
Seth McCarthy <seth@letshighlight.com>
2022-12-30 11:02:05 -0800

    Remove tests from FunctionTests
    Make this just be a safety net of a single sociable test.  It
    confirms that the intermediate pieces get wired together.

--- SendEmail/SendEmail.Mocks.Tests/FunctionTests.cs
+++ SendEmail/SendEmail.Mocks.Tests/FunctionTests.cs
@@ -19,46 +19,17 @@ public class FunctionTests
         _emailService = new Mock<IAmazonSimpleEmailService>();
         _function = new Function(_emailService.Object);
     }
-    
+
     [Test]
-    public async Task EmptyBatchDoesNotFail()
+    public async Task GetsMessagesToEmailService()
     {
-        var input = new SQSEvent { Records = new List<SQSEvent.SQSMessage>() };
+        var input = new SQSEvent { Records = CreateFromRequests(new[] { CreateRequest() }) };
 
         var result = await _function.FunctionHandler(input, new TestLambdaContext());
         
         Assert.That(result.BatchItemFailures, Is.Empty);
+        _emailService.Verify(e=>e.SendTemplatedEmailAsync(It.IsAny<SendTemplatedEmailRequest>(), It.IsAny<CancellationToken>()), Times.Exactly(input.Records.Count));
     }
-
-    [Test]
-    public async Task BatchWithMultipleMessagesForwardsEachToEmailService()
-    {
-        var requests = new[] { CreateRequest(), CreateRequest(), CreateRequest() };
-        var input = new SQSEvent { Records = CreateFromRequests(requests) };
-
-        var result = await _function.FunctionHandler(input, new TestLambdaContext());
-        
-        Assert.That(result.BatchItemFailures, Is.Empty);
-        foreach (var expectedRequest in requests)
-        {
-            _emailService.Verify(e=>e.SendTemplatedEmailAsync(It.Is<SendTemplatedEmailRequest>(r=>r.Source == expectedRequest.Source), It.IsAny<CancellationToken>()));
-        }
-    }
-
-    [Test]
-    public async Task BatchWithSomeExceptionsOnlyFailsThoseRequests()
-    {
-        var requests = new[] { CreateRequest(), CreateRequest(), CreateRequest() };
-        var input = new SQSEvent { Records = CreateFromRequests(requests) };
-        _emailService.Setup(e =>
-            e.SendTemplatedEmailAsync(It.Is<SendTemplatedEmailRequest>(r => r.Source == requests[1].Source),
-                It.IsAny<CancellationToken>())).Throws<Exception>();
-
-        var result = await _function.FunctionHandler(input, new TestLambdaContext());
-        
-        Assert.That(result.BatchItemFailures.Single().ItemIdentifier, Is.EqualTo(input.Records[1].MessageId));
-        _emailService.Verify(e=>e.SendTemplatedEmailAsync(It.IsAny<SendTemplatedEmailRequest>(), It.IsAny<CancellationToken>()), Times.Exactly(requests.Length));
-    }    
 
     private SendTemplatedEmailRequest CreateRequest()
     {
