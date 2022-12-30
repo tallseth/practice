ff1805172d8eefd6d02f6cca1561fb00564b0261
Seth McCarthy <seth@letshighlight.com>
2022-12-30 07:35:05 -0800

    Refactor EmailWrapper out of an abstract class

--- SendEmail/SendEmail.Nullables/EmailWrapper.cs
+++ SendEmail/SendEmail.Nullables/EmailWrapper.cs
@@ -4,47 +4,41 @@ using SendEmail.Nullables.OutputTracking;
 
 namespace SendEmail.Nullables;
 
-public abstract class EmailWrapper
+public class EmailWrapper
 {
-    public abstract Task<SendTemplatedEmailResponse> SendTemplatedEmail(SendTemplatedEmailRequest request);
-    public abstract OutputTracker<SendTemplatedEmailRequest> TrackRequests();
+    private Func<SendTemplatedEmailRequest, CancellationToken, Task<SendTemplatedEmailResponse>> _function;
+    private readonly OutputListener<SendTemplatedEmailRequest> _outputListener = new();
+
+    private EmailWrapper(Func<SendTemplatedEmailRequest, CancellationToken, Task<SendTemplatedEmailResponse>> function)
+    {
+        _function = function;
+    }
+
+    public Task<SendTemplatedEmailResponse> SendTemplatedEmail(SendTemplatedEmailRequest request)
+    {
+        _outputListener.Observe(request);
+        return _function(request, CancellationToken.None);
+    }
+
+    public OutputTracker<SendTemplatedEmailRequest> TrackRequests()
+    {
+        return _outputListener.CreateTracker();
+    }
  
     public static EmailWrapper Create()
     {
         var aws = new AmazonSimpleEmailServiceClient();
-        return new FunctionImpl(aws.SendTemplatedEmailAsync);
+        return new EmailWrapper(aws.SendTemplatedEmailAsync);
     }
     
     public static EmailWrapper CreateNull(SendTemplatedEmailResponse? configuredResponse=null, Exception? configuredException=null)
     {
-        return new FunctionImpl((_, _) =>
+        return new EmailWrapper((_, _) =>
         {
             if (configuredException != null)
                 throw configuredException;
 
             return Task.FromResult(configuredResponse ?? new SendTemplatedEmailResponse());
         });
-    }
-
-    private class FunctionImpl : EmailWrapper
-    {
-        private Func<SendTemplatedEmailRequest, CancellationToken, Task<SendTemplatedEmailResponse>> _function;
-        private readonly OutputListener<SendTemplatedEmailRequest> _outputListener = new();
-
-        public FunctionImpl(Func<SendTemplatedEmailRequest, CancellationToken, Task<SendTemplatedEmailResponse>> function)
-        {
-            _function = function;
-        }
-
-        public override Task<SendTemplatedEmailResponse> SendTemplatedEmail(SendTemplatedEmailRequest request)
-        {
-            _outputListener.Observe(request);
-            return _function(request, CancellationToken.None);
-        }
-
-        public override OutputTracker<SendTemplatedEmailRequest> TrackRequests()
-        {
-            return _outputListener.CreateTracker();
-        }
     }
 }
